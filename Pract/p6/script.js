7ee55ba34a65d813a9cceea3115e9f5025ddaeb4
nammulovett <nammmu@gmail.com>
2022-09-28 21:42:54 +0200

    modify

--- Pract/p6/script.js
+++ Pract/p6/script.js
@@ -4,6 +4,7 @@ let randomNum = parseInt(Math.random() * 1001);
 document.write(`<h2>${randomNum}</h2>`);
 
 let num = 0;
+let contador = 0;
 num = prompt('Adivina el número comprendido entre 1 y 1000'); //Le solicita un número al usuario
 
 while (isNaN(num)) {
@@ -11,7 +12,7 @@ while (isNaN(num)) {
   alert(`${num} no es un número`);
   num = prompt('Introduce un número');
 }
-let contador = 0;
+
 while (num != randomNum) {
   num > randomNum ? alert('El número es menor') : alert('El número es mayor');
   contador++;
