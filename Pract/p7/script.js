fe2e3587f03ef8da9f196eb41128af103e911287
mglora <10695@cifpceuta.es>
2022-09-30 13:01:55 +0200

    modify p7 refactor

--- Pract/p7/script.js
+++ Pract/p7/script.js
@@ -2,31 +2,34 @@
 
 let userDni = prompt('Por favor introduce tu DNI');
 let numUserDni = userDni.substring(0, 8);
+let userLetra = userDni.substring(8, 9);
 
 while (
   userDni.length != 9 || //No puede tener menos de 9 caracteres.
   userDni.length === 0 || // No puede estar vacío = 0
-  isNaN(numUserDni) || //los 8 primeros caracteres tiene qu eser un número
-  !isNaN(userDni.substring(8, 9)) // el último caracter debe ser una letra
+  isNaN(numUserDni) || //los 8 primeros caracteres tiene que ser un número
+  !isNaN(userLetra) // el último caracter debe ser una letra
 ) {
   userDni = prompt(
     'Por favor introduce tu DNI válido, debe tener los 9 caracteres, 8 números y una letra válida al final'
   );
 }
 
-const letraM = 'TRWAGMYFPDXBNJZSQVHLCKE';
-const letra = 'trwagmyfpdxbnjzsqvhlcke';
-let pos = numUserDni % 23;
+//Si hemos llegado hasta aquí significa que vamos bien y necesitamos comprobar ahora que la letra es correcta.
 
-if (
-  userDni.substring(8, 9) == letra.charAt(pos) ||
-  userDni.substring(8, 9) == letraM.charAt(pos)
-) {
+const letra = 'TRWAGMYFPDXBNJZSQVHLCKE'; //letras que debe tener el DNI en el orden
+let pos = numUserDni % 23; //El resto nos indica la posición de la letra anterior
+
+if (userLetra.toUpperCase == letra.charAt(pos)) {
+  //Si la letra del usuario, forzando mayúscula es igual a la posición
   document.write(
     `<h2>La validación de tu DNI es correcta, tu DNI es ${userDni} </h2>`
   );
 } else {
-  `<h2>La letra de tu DNI no coincide, tu DNI debería ser ${numUserDni}${letra.charAt(
-    pos
-  )}${letraM.charAt(pos)} </h2>`;
+  //De lo contrario, te corrige el dni
+  document.write(
+    `(<h2>La letra de tu DNI no coincide, tu DNI debería ser ${numUserDni}${letra.charAt(
+      pos
+    )}) </h2>`
+  );
 }
