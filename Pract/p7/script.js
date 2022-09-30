e4e2082d2d7bfa738f0d3afedd9f2d884ce706df
mglora <10695@cifpceuta.es>
2022-09-30 12:50:07 +0200

    modified p7

--- Pract/p7/script.js
+++ Pract/p7/script.js
@@ -14,17 +14,19 @@ while (
   );
 }
 
-function letraDNI(numUserDni) {
-  let letra = 'TRWAGMYFPDXBNJZSQVHLCKE';
-  let pos = dniSinLetra % 23;
+const letraM = 'TRWAGMYFPDXBNJZSQVHLCKE';
+const letra = 'trwagmyfpdxbnjzsqvhlcke';
+let pos = numUserDni % 23;
 
-  if (userDni == numUserDni + letra.charAt(pos)) {
-    return document.write(
-      `<h2>La validación de tu DNI es correcta, tu DNI es ${userDni}</h2>`
-    );
-  } else {
-    return `<h2>La letra de tu DNI no coincide, tu DNI debería ser ${numUserDni} + letra.charAt(pos)</h2>`;
-  }
+if (
+  userDni.substring(8, 9) == letra.charAt(pos) ||
+  userDni.substring(8, 9) == letraM.charAt(pos)
+) {
+  document.write(
+    `<h2>La validación de tu DNI es correcta, tu DNI es ${userDni} </h2>`
+  );
+} else {
+  `<h2>La letra de tu DNI no coincide, tu DNI debería ser ${numUserDni}${letra.charAt(
+    pos
+  )}${letraM.charAt(pos)} </h2>`;
 }
-
-letraDNI(numUserDni);
