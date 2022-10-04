f9e9b186edeaeb73ff466adb84f6424b8ed10ec9
nammulovett <nammmu@gmail.com>
2022-10-04 20:58:03 +0200

    add other way to do the p8

--- Pract/p1eval/p8/script.js
+++ Pract/p1eval/p8/script.js
@@ -1,14 +1,12 @@
-'use strict';
-
 /* El cifrado César consiste en tomar cada letra de un mensaje y desplazarla en el alfabeto el número que diga una clave. Por ejemplo, la letra A desplazada con clave 2, se convertirá en C. Aunque el cifrado César funciona con un alfabeto completo y por desplazamiento (en el ejemplo anterior la letra Z se suele convertir en B) se pide crear una página que pida al usuario un texto y una clave y escriba el mismo texto, pero desplazando los caracteres en la tabla Unicode el número que indique la clave. */
-var num = 0;
-var frase = '';
+let key = 0;
+let frase = '';
 
-var fraseNueva = '';
-var suma = 0;
+let pswd = '';
+let suma = 0;
 
-while (num < 1 || num > 100) {
-  num = prompt('Escribe el valor de cifrado entre 1-100');
+while (key < 1 || key > 100) {
+  key = prompt('Escribe el valor de cifrado entre 1-100');
 }
 while (frase.valueOf() == '') {
   frase = prompt('Escribe la cadena a descifrar');
@@ -16,15 +14,15 @@ while (frase.valueOf() == '') {
 
 for (i = 0; i < frase.length; i++) {
   //En este for lo que hacemos es crear la frase sumando el valor que nos ha dado en su caracter unicode
-
   //frase.charCodeAt(i) = parseInt(frase.charCodeAt(i)) + parseInt(num);
-  console.log(frase.charCodeAt(i));
-  console.log(parseInt(frase.charCodeAt(i)) + parseInt(num));
 
-  fraseNueva += String.fromCharCode(frase.charCodeAt(i) + parseInt(num));
-  //fraseNueva = String.fromCharCode(fraseNueva);
+  console.log(frase.charCodeAt(i));
+  console.log(parseInt(frase.charCodeAt(i)) + parseInt(key));
+
+  pswd += String.fromCharCode(frase.charCodeAt(i) + parseInt(key));
+  //pswd = String.fromCharCode(pswd);
 }
 
 //console.log(frase);
 
-alert('La cadena descifrada es... ' + fraseNueva);
+alert(`La nueva contraseña es ${pswd}`);
