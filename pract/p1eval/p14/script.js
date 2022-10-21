cee7bdb7ad24ef162008ad89ea15864496d9313e
mglora <10695@cifpceuta.es>
2022-10-21 13:37:20 +0200

    corrección

--- pract/p1eval/p14/script.js
+++ pract/p1eval/p14/script.js
@@ -6,7 +6,7 @@ function cambiaColor() {
   let z = parseInt(Math.random() * 256);
   //Generamos tres números aleatorios para r g b
   let bgColor = `rgb(${x},${y},${z})`; //meto los núneros en una variable para que sea más sencillo de manejar
-  document.body.style.background = bgColor;
+  document.body.style.background = bgColor; //modificamos el objeto body style background y recibe el número aleatorio
 }
 
 setInterval(cambiaColor, 1000);
