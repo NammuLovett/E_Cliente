7c34fa8465aa228894bcca2926472ea2ecd69888
nammulovett <nammmu@gmail.com>
2022-10-25 14:47:34 +0200

    .

--- pract/p1eval/p14/script.js
+++ pract/p1eval/p14/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function cambiaColor() {
+  let x = parseInt(Math.random() * 256);
+  let y = parseInt(Math.random() * 256);
+  let z = parseInt(Math.random() * 256);
+  //Generamos tres números aleatorios para r g b
+  let bgColor = `rgb(${x},${y},${z})`; //meto los núneros en una variable para que sea más sencillo de manejar
+  document.body.style.background = bgColor; //modificamos el objeto body style background y recibe el número aleatorio
+}
+
+setInterval(cambiaColor, 2000);
