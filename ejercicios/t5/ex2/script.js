460eacb11826570489ba9d595b28798d1b2cd350
NammuLovett <nammmu@gmail.com>
2022-11-15 13:58:04 +0100

    ex4

--- ejercicios/t5/ex2/script.js
+++ ejercicios/t5/ex2/script.js
@@ -1,3 +1,13 @@
+/*
+Ejercicio 2.- Ampliemos el ejemplo anterior con el constructor Coche:
+o Funciones setters y getters para todas las propiedades. Las setters asignan un valor que le enviamos
+como argumento, mientras que las getter devuelven el valor asignado.
+o Modifica el método rellenarDepósito para que use uno de dichos métodos (setCantidad p.e.).
+o Método muestraConf que muestre por consola y en un formato adecuado todas las propiedades
+(por supuesto, usando los métodos creados).
+o Crea un objeto a partir del constructor y prueba el funcionamiento del programa.
+*/
+
 /*---------------------------------------------------------------*/
 
 function Coche(marca, combustible) {
---
+++ ejercicios/t5/ex5/script.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio 5.- Herencia con constructores
+Define un tipo de objeto Cometa cuyas propiedades de instancia (específicas de cada objeto) sean
+diametro, temperatura y nombre. La temperatura será un valor numérico que suponemos está en grados
+centígrados.
+Como propiedad común a todos los objetos usaremos def (que contendrá la definición de cometa según el
+diccionario) y como métodos comunes getRadio() (que debe devolver el radio) y getTemp() (que debe
+devolver el valor de temperatura expresado en grados Farenheit). Para la conversión de centígrados a
+Farenheit podrás seguir la fórmula: oF = (oC · 1,8) + 32
+Crea tres objetos de tipo cometa y comprueba que puedes acceder tanto a las propiedades específicas
+como a las propiedades y métodos comunes desde cada objeto.
+
+ */
+
+/*---------------------------------------------------------------*/
