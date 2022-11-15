a0e8bfddf736f2aab5a2d3fdfe7c223e73553df0
nammulovett <nammmu@gmail.com>
2022-11-15 19:53:47 +0100

    ex14

--- pract/p1eval/p14/script.js
+++ pract/p1eval/p14/script.js
@@ -1,6 +1,9 @@
 /*
 
-
+La función Fibonacci está relacionada con una sucesión de elementos donde los dos
+primeros son el cero y el uno, y el resto son la suma de sus dos anteriores. Ejemplo:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
+✓ Así si pasamos el número 10, devolvería 55.
 
 
 */
