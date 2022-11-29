e4a43ef7b7173da168b87b0486f1eb35e4e4eb2f
nammulovett <nammmu@gmail.com>
2022-11-29 13:40:08 +0100

    .

---
+++ ejercicios/t6/ex1/script.js
@@ -0,0 +1,9 @@
+/*
+
+Ejercicio 1.- Mediante la propiedad innerHTML, muestra por pantalla la siguiente información: ¿están
+las cookies activadas en el navegador? ¿y java? ¿Qué lenguaje usa? ¿está el navegador trabajando online?
+Muestra además si nuestro navegador usa el motor Gecko, para ello deberás sacar la información del string
+que devuelve la propiedad userAgent.
+
+*/
+console.log(document.innerHTML());
