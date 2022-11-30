3463510f4a72be853afa452faa615c15742a83c0
NammuLovett <nammmu@gmail.com>
2022-11-30 12:26:04 +0100

    .

--- ejercicios/t6/ex7/script.js
+++ ejercicios/t6/ex7/script.js
@@ -2,10 +2,24 @@
 450 con un texto en negrita donde aparezcan escritas las dimensiones de la ventana actual y de la pantalla
 del usuario. Al pasar 10 segundos la ventana creada se cerrará. */
 
+/* window.onload = updateClock;
+
+var totalTime = 10;
+
+function updateClock() {
+  document.getElementById('countdown').innerHTML = totalTime;
+  if (totalTime == 0) {
+    alert('Final');
+  } else {
+    totalTime -= 1;
+    setTimeout('updateClock()', 1000);
+  }
+}
+ */
 let ventana = () => {
   //metemos en una variable para abrir la ventana
   let vent = window.open('', '', 'width=300,height=450');
-  vent.document.write('Esta ventana se cerrará en 5 segundos');
+  vent.document.write("<span id='countdown'></span>");
 
   //hay que meter en una function el .close para que funcione
   setTimeout(function () {
