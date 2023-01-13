db739c59e5d3a4b95927963a42d0d81ad489befd
NammuLovett <nammmu@gmail.com>
2023-01-13 11:46:23 +0100

    fix prob

--- pract/p2eval/mgomez_p03eva2/script.js
+++ pract/p2eval/mgomez_p03eva2/script.js
@@ -1,5 +1,5 @@
 // Se crea un nuevo elemento 'lista'.
-let newList = document.createElement('lista');
+let newList = document.createElement('li');
 // Se crea el contenido del elemento.
 newList.innerHTML = 'Soy el nuevo párrafo.';
 
@@ -7,15 +7,15 @@ newList.innerHTML = 'Soy el nuevo párrafo.';
 let position = prompt('Introduce un nuevo párrafo en la posición (1 - 10)');
 
 // Expresión regular para validar lo recibido del usuario.
-let regex = /(^[1-9]$)|(^10$)|(^null$)/; // Número entre 1 y 9; o 10;o null.
+let regex = /(^[1-9]$)|(^10$)|(^null$)/; // Filtra: Número entre 1 y 9 | 10 | null.
 
 while (!regex.test(position)) {
-  // Mientras pos no concuerde con la expresión regular.
+  // Mientras que position no sea un el filtro del regex, pregunta .
   position = prompt('La posición debe estar entre 1 y 10.'); // Se pregunta de nuevo.
 }
 
 if (position != null) {
   // Si pos no es null
-  let lista = document.querySelectorAll('#ol lista')[position - 1]; // Selecciona el párrafo que va a ser el siguiente al que vamos a introducir.
-  document.getElementById('ol').insertBefore(newList, lista); // Introduce el nuevo párrafo antes.
+  let lista = document.querySelectorAll('#lista li')[position - 1]; // Selecciona el párrafo que va a ser el siguiente al que vamos a introducir.
+  document.getElementById('lista').insertBefore(newList, lista); // Introduce el nuevo párrafo antes.
 }
