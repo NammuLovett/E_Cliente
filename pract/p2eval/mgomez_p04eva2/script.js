11be533bb871b0a53a9de675d8a6dfbd125e0431
NammuLovett <nammmu@gmail.com>
2023-01-13 14:08:54 +0100

    fix

--- pract/p2eval/mgomez_p04eva2/script.js
+++ pract/p2eval/mgomez_p04eva2/script.js
@@ -1,9 +1,11 @@
-var parrafos = document.getElementsByTagName('p');
-var palabras = 0;
+let parrafos = document.getElementsByTagName('p'); //obtenemos un array con los párrafos
+
+let palabras = 0; //inicializamos una variable para contar
 
 for (let p of parrafos) {
-  var text = p.textContent;
-  palabras = palabras + text.split(' ').length;
+  //for of para iterar y sumar las palabras
+  let text = p.textContent; //transforma p en string
+  palabras += text.split(' ').length; //suma las palabras quitando los espacios
 }
 
 console.log(palabras);
