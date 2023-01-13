fc03e02f8628782ba276ecf9ff7dceb117983d23
NammuLovett <nammmu@gmail.com>
2023-01-13 18:31:31 +0100

    ejercicio terminado

--- pract/p2eval/mgomez_p04eva2/script.js
+++ pract/p2eval/mgomez_p04eva2/script.js
@@ -8,4 +8,14 @@ for (let p of parrafos) {
   palabras += text.split(' ').length; //suma las palabras quitando los espacios
 }
 
+let main = document.getElementById('parrafos'); //  Identifica el contenedor de los parrafos
+
+let mostrarContador = document.createElement('p'); // Crea un párrafo.
+mostrarContador.innerText = `Hay un total de ${palabras}`; // El párrafo contiene el nº de palabras.
+mostrarContador.style.fontWeight = 'bold'; // Se le aplica un estilo al párrafo.
+main.insertBefore(
+  mostrarContador,
+  document.querySelector('#parrafos p:first-child')
+); // Se inserte el párrafo al inicio del dom.
+
 console.log(palabras);
