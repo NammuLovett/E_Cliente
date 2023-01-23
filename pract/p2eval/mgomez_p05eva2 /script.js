c985e414948731fd3eb05eafc518b1f8186a6920
NammuLovett <nammmu@gmail.com>
2023-01-23 18:07:21 +0100

    .

--- pract/p2eval/mgomez_p05eva2 /script.js
+++ pract/p2eval/mgomez_p05eva2 /script.js
@@ -1,31 +1,29 @@
-// Muestra la estructura en árbol de la página.
-function mostrarArbol(root) {
-  console.log(`Elemento ${root.nodeName}`); // Muestra el nombre del elemento actual.
+//Recogemos la etiqueta HTML
+let html = document.getElementsByTagName('html')[0];
 
-  // Si el elemento posee atributos, se itera mostrando su nombre y valor.
-  if (root.attributes.length > 0) {
-    console.log(`Atributos de ${root.nodeName}: `);
-    for (let atr of root.attributes) {
-      console.log(`${atr.name} -> ${atr.value}`);
-    }
-  }
-  // Si el elemento posee childs, se nombran y muestra su información
-  if (root.children.length > 0) {
-    console.log(`Hijos de ${root.nodeName} : `);
-    // Primero muestra todos los hijos.
-    for (let child of root.children) {
-      console.log(child.nodeName);
-    }
-    console.log(' ');
-    // Información de cada child
-    for (let child of root.children) {
-      mostrarArbol(child);
-      console.log(' ');
+//Llamamos a la función recorre que se encarga de recorrer todos los elementos del html a partir del indicado por parámetro
+recorre(html);
+
+function recorre(elemento) {
+  //Recorremos todos los hijos
+  for (let i = 0; i < elemento.children.length; i++) {
+    //Almacenamos los hijos en una variable
+    let hijo = elemento.children[i];
+
+    //Recorremos los hijos de los hijos
+    recorre(hijo);
+
+    //En caso de que tenga atributos, los mostramos; en caso de que no, mostramos solo el nombre de la etiqueta
+    if (hijo.hasAttributes()) {
+      //Recogemos el nombre de los atributos de la etiqueta
+      for (let nombre of hijo.getAttributeNames()) {
+        //Recogemos el valor a partir del nombre
+        //nombre -> id | valor -> 1 POR EJEMPLO
+        let valor = hijo.getAttribute(nombre);
+        console.log(hijo.nodeName, '=>', nombre, ' ', valor);
+      }
+    } else {
+      console.log(hijo.nodeName);
     }
   }
 }
-
-let html = document.getElementsByTagName('html')[0]; // De donde parte para ir mostrando la info.
-
-//Function
-mostrarArbol(html);
