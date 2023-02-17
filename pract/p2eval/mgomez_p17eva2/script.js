d764c2aef066031981074158b48862e0cb7a2edf
NammuLovett <nammmu@gmail.com>
2023-02-17 18:10:05 +0100

    añadir comentarios

--- pract/p2eval/mgomez_p17eva2/script.js
+++ pract/p2eval/mgomez_p17eva2/script.js
@@ -1,4 +1,9 @@
 // Módulo Formas que contiene los métodos para crear círculos y cuadrados
+
+/* La primera parte del código define el módulo Formas que contiene dos métodos: 
+crearCirculo y crearCuadrado. Ambos métodos reciben dos argumentos (x e y) que se utilizan para definir 
+la posición de la forma en el contenedor. El método crearCirculo crea un elemento div que representa un círculo 
+y le asigna un tamaño aleatorio y un color de fondo aleatorio. El método crearCuadrado hace lo mismo pero para un cuadrado. */
 const Formas = {
   crearCirculo: function (x, y) {
     const circulo = document.createElement('div');
@@ -27,6 +32,14 @@ const Formas = {
 };
 
 // Evento para el ratón que crea formas (círculos o cuadrados) y los añade al contenedor
+
+/* Añade un EventListener para el evento mousedown (cuando se presiona el botón del ratón). 
+Dentro del evento, se verifica el botón que se ha presionado: si es el botón izquierdo (0), 
+se crea un círculo llamando al método crearCirculo y se añade al contenedor con appendChild. 
+Si es el botón derecho (2), se crea un cuadrado llamando al método crearCuadrado y se añade al contenedor. 
+Si se presiona cualquier otro botón, se lanza una excepción y se registra un mensaje de error en la consola. 
+En ambos casos se utiliza el objeto event para obtener las coordenadas (clientX y clientY) del puntero del ratón y pasarlas como argumentos a los métodos crearCirculo y crearCuadrado. */
+
 document.addEventListener('mousedown', function (event) {
   try {
     if (event.button == 0) {
